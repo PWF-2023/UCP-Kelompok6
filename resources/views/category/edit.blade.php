32cb45d4713fc45dc86701379af8ca4c19ee6aaf
ZahwaLadyaMaharani <zahwa.ladya.ft20@mail.umy.ac.id>
2023-05-25 23:48:24 +0700

    update halaman edit

--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form method="post" action="" class="">
+                    <form method="post" action="{{ route('category.update,$category) }}" class="">
                         @csrf
                         @method('patch')
                         <div class="mb-6">
