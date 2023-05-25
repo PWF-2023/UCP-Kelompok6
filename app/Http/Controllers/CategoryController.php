610e98bef36f52b603800382a19cfb7d56a3d818
priyaihsan <priyaihsan771@gmail.com>
2023-05-25 20:39:30 +0700

    update category controller CRUD

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,51 +9,68 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        
-       
-        return view('category.index');
-        
+        //todo , title
+        $categories = Category::where('user_id', auth()->user()->id)
+            ->get();
+        // dd($categories);
+
+        return view('category.index', compact('categories'));
     }
 
-    public function create()
+    public function create(Request $request)
     {
-        
-       
-        return view('category.create');
-        
+        //
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+        $category = Category::create([
+            'title' => ucfirst($request->title),
+            'user_id' => auth()->user()->id,
+        ]);
+        return redirect()->route('category.index')->with('success', 'Category created successfully!');
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        
-       
-        return view('category.store');
-        
+        //
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+        $category = Category::create([
+            'title' => ucfirst($request->title),
+            'user_id' => auth()->user()->id,
+        ]);
+        return redirect()->route('category.index')->with('success', 'Category created successfully!');
     }
 
-    public function edit()
+    public function edit(Category $category)
     {
-        
-       
-        return view('category.edit');
-        
+        //
+        if (auth()->user()->id = $category->user_id) {
+            return view('category.edit', compact('category'));
+        }
+        return redirect()->route('category.index')->with('danger', 'You are not authorized to edit this Category!');
     }
 
-    public function update()
+    public function update(Request $request, Category $category)
     {
-        
-       
-        return view('category.update');
-        
-    }
-    
-    public function destroy()
-    {
-        
-       
-        return view('category.destroy');
-        
+        //
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+        $category->update([
+            'title' => ucfirst($request->title),
+        ]);
+        return redirect()->route('category.index')->with('success', 'Category updated successfully!');
     }
 
-    
+    public function destroy(Category $category)
+    {
+        //
+        if(auth()->user()->id == $category->user_id){
+            $category->delete();
+            return redirect()->route('category.index')->with('success','Category deleted successfully!');
+        }
+        return redirect()->route('category.index')->with('danger','You are not authorized to delete this Category!');
+    }
 }
